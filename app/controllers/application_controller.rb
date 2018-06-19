2fa64d2a8670f1c656c88f72d06a93e7912bb22e
Nicholas Powers <njpowers7915@gmail.com>
2018-06-19 01:34:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,52 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, 'secret'
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :home
+  end
+
+  get '/index' do
+    erb :index
+  end
+
+  get '/signup' do
+    @user = Helpers.current_user(session)
+    if Helpers.is_logged_in?(session) == false
+        erb :'/users/signup'
+    else
+        erb :error
+    end
+  end
+
+  post "/signup" do
+      @user = User.new(email: params[:email], username: params[:username], password: params[:password])
+      if params[:username] == "" || params[:password] == "" || params[:email] == ""
+          redirect "/signup"
+      else
+          @user.save
+          session[:id] = @user.id
+          redirect "/tweets/tweets"
+      end
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/sessions' do
+    @user = User.find_by(username: params["username"], password: params["password"])
+    if @user.nil?
+        redirect '/login'
+    else
+        session[:id] = @user.id
+        redirect '/users/show'
+    end
   end
 
 end
